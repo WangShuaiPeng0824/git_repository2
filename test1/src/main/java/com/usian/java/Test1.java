f0cd83b2c447dc1f260c98a4f7178524be4e7571
WangShuaiPeng0824 <65169503+WangShuaiPeng0824@users.noreply.github.com>
2020-05-13 22:00:45 +0800

    Update Test1.java

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -3,6 +3,7 @@ package com.usian.java;
 public class Test1 {
     //修改test1
     public static void main(String[] args) {
-
+        //github端（其他同事）的修改
+        Sysout.out.println("世界你好");
     }
 }
